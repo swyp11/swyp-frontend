409b634867f0781cbef06064bfe1733aefe9f7a4
Ahnyeongjun <toinin@naver.com>
2025-11-05 00:30:49 +0900

    fix: src 폴더 추가

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -25,17 +25,17 @@ const roboto = Roboto({
 const pretendard = localFont({
   src: [
     {
-      path: "../node_modules/pretendard/dist/web/static/woff2/Pretendard-Regular.woff2",
+      path: "../../node_modules/pretendard/dist/web/static/woff2/Pretendard-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../node_modules/pretendard/dist/web/static/woff2/Pretendard-Medium.woff2",
+      path: "../../node_modules/pretendard/dist/web/static/woff2/Pretendard-Medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../node_modules/pretendard/dist/web/static/woff2/Pretendard-SemiBold.woff2",
+      path: "../../node_modules/pretendard/dist/web/static/woff2/Pretendard-SemiBold.woff2",
       weight: "600",
       style: "normal",
     },
---
+++ frontend/src/components/schedule/NavigationHeader.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+
+interface NavigationHeaderProps {
+    onPrev: () => void;
+    onNext: () => void;
+    title: string;
+    showDropdown?: boolean;
+}
+
+export const NavigationHeader: React.FC<NavigationHeaderProps> = ({
+    onPrev,
+    onNext,
+    title,
+    showDropdown = true,
+}) => {
+    return (
+        <div className="flex items-center justify-between px-2 pb-5">
+            <button
+                onClick={onPrev}
+                className="w-10 h-10 bg-surface-2 rounded-lg flex items-center justify-center"
+            >
+                <Image
+                    className="relative w-6 h-6"
+                    alt=""
+                    src="/img/chevron_backward.svg"
+                    width={24}
+                    height={24}
+                />
+            </button>
+
+            <button className="bg-surface-2 rounded-lg px-5 py-2 flex items-center gap-1">
+                <span className="title-2 text-on-surface">{title}</span>
+                {showDropdown && (
+                    <Image
+                        className="relative w-6 h-6"
+                        alt=""
+                        src="/img/keyboard-arrow-down.svg"
+                        width={24}
+                        height={24}
+                    />
+                )}
+            </button>
+
+            <button
+                onClick={onNext}
+                className="w-10 h-10 bg-surface-2 rounded-lg flex items-center justify-center"
+            >
+                <Image
+                    className="relative w-6 h-6 rotate-180"
+                    alt=""
+                    src="/img/chevron_backward.svg"
+                    width={24}
+                    height={24}
+                />
+            </button>
+        </div>
+    );
+};
